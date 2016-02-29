d5d3e1e00dff6ebe992ccbba38daf0d2fb010ff2
Mark Savage <mark@savagebits.com>
2016-02-29 08:25:13 -0600

    added a class

--- app/src/feature/feature.ts
+++ app/src/feature/feature.ts
@@ -42,3 +42,33 @@ let feature2 = <IFeature>{
   name: 'Newer feature',
   description: this.name + ' is a feature'
 }
+
+//var productType = new ProductType('A product');
+class ProductType {
+  constructor(public description: string) {}
+}
+
+//var feature = new Feature('Feature A', productType);
+class Feature {
+  private _name: string;
+  private _productType: ProductType;
+  
+  constructor(name: string, productType?: ProductType) {
+    this.name = name;
+    if (productType) {
+      this._productType = productType;
+    }
+  }
+  
+  get name(): string {
+    return this._name;
+  }
+  
+  set name(value: string) {
+    this._name = value;
+  }
+  
+  beginWork() : void {
+    alert('Work has begun on ' + this.name);
+  }
+}
--- bundle.js
+++ bundle.js
@@ -29,3 +29,33 @@ var feature2 = {
     name: 'Newer feature',
     description: this.name + ' is a feature'
 };
+//var productType = new ProductType('A product');
+var ProductType = (function () {
+    function ProductType(description) {
+        this.description = description;
+    }
+    return ProductType;
+}());
+//var feature = new Feature('Feature A', productType);
+var Feature = (function () {
+    function Feature(name, productType) {
+        this.name = name;
+        if (productType) {
+            this._productType = productType;
+        }
+    }
+    Object.defineProperty(Feature.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        set: function (value) {
+            this._name = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Feature.prototype.beginWork = function () {
+        alert('Work has begun on ' + this.name);
+    };
+    return Feature;
+}());
