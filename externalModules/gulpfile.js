b428517aa42753703426f8d7e41d42375fbb3e6d
Mark Savage <mark@savagebits.com>
2016-03-05 12:16:39 -0600

    external modules w require.js cleanup

--- externalModules/gulpfile.js
+++ externalModules/gulpfile.js
@@ -16,8 +16,7 @@ var config = {
     tsConfig: './tsconfig.json',
     html: './index.html',
     js: './app/src/**/*.js',
-    ts: './app/src/**/*.ts',
-    bundle: './bundle.js'    
+    ts: './app/src/**/*.ts'
   }
 }
 
@@ -31,26 +30,21 @@ gulp.task('open', ['connect'], function() {
 
 gulp.task('connect', function() {
   connect.server({
-    root: ['.'],
+    root: [config.paths.rootDirectory],
     port: config.port,
-    base: config.devBaseUrl,
+    base: config.devBaseUrl + config.parentDirectory,
     fallback: 'index.html',
     livereload: true
   });
 });
 
-//could compile TypeScript to ES6 and then
-// transpile to ES5 using babel by changing
-// target to 'ES6'
 gulp.task('ts', function() {
-  //pulling tsConfig in here to mirror settings between inspection and compile
   var tsConfig = JSON.parse(fs.readFileSync(config.paths.tsConfig,'utf8'));  
     
   var newTsConfig = Object.assign(    
     tsConfig.compilerOptions
   );
     
-  //generates individual module directories containing .ts compiled to .js
   gulp.src(config.paths.ts)
     .pipe(typescript(newTsConfig))
     .pipe(gulp.dest(config.paths.distDirectory));     
